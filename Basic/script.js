a2093fffeab3cc13b65dcc315e507fad6bec1db0
Mohit Pandey <144821450+Mohitpandey7797@users.noreply.github.com>
2024-06-10 00:24:00 +0530

    Delete Basic directory

--- Basic/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('menuIcon').addEventListener('click', function() {
-    const navBox = document.getElementById('navBox');
-    if (navBox.style.display === "flex") {
-        navBox.style.display = "none";
-        
-    } else {
-        navBox.style.display = "flex";
-    }
-});
